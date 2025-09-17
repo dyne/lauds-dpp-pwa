3817823e3cddec54aa3f00093a3414a5967ec8a6
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2025-09-17 18:31:56 +0200

    change Reflow to Lauds

--- pages/api/verify.js
+++ pages/api/verify.js
@@ -11,7 +11,7 @@ Given I have a 'signature' named 'DPP.signature'
 
 When I verify the 'DPP' has a signature in 'DPP.signature' by 'https://interfacer-staging.dyne.im/api/json/trace'
 
-Then print the string 'The Reflow DPP signature was verified'
+Then print the string 'The LAUDS DPP signature was verified'
 Then print the 'DPP'
 Then print the 'DPP.signature'`
   const keys = '{"https://interfacer-staging.dyne.im/api/json/trace":{"public_key":"BBCQg21VcjsmfTmNsg+I+8m1Cm0neaYONTqRnXUjsJLPa8075IYH+a9w2wRO7rFM1cKmv19Igd7ntDZcUvLq3xI="}}';
